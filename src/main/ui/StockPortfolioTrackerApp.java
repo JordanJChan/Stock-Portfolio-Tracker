9427b7b3202b3b7efe6a4438e02a4b817c4a7d05
Jordan Chan <ijordanc@students.cs.ubc.ca>
2026-02-15 12:41:31 -0800

    User can now buy and sell shares

--- src/main/ui/StockPortfolioTrackerApp.java
+++ src/main/ui/StockPortfolioTrackerApp.java
@@ -42,9 +42,47 @@ public class StockPortfolioTrackerApp {
             } else if (userNumberInput == 2) { // View companies in portfolio
                 displayCompanies();
             } else if (userNumberInput == 3) { // Buy more shares in a company
+                if (userPortfolio.getCompanies().size() == 0) {
+                    System.out.println("You have not added any companies. Please add a company first.");
+                } else {
+                    displayCompanies();
+                    System.out.print("Enter the company number you want to buy more shares in: ");
+                    userNumberInput = input.nextInt();
+                    input.nextLine();
 
+                    System.out.print("How many shares? ");
+                    int numberOfShares = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("What is the price of each share: ");
+                    int price = input.nextInt();
+                    input.nextLine();
+
+                    addShares(userPortfolio.getCompanies().get(userNumberInput-1), numberOfShares, price);
+                }
             } else if (userNumberInput == 4) { // Sell shares in a company
+                if (userPortfolio.getCompanies().size() == 0) {
+                    System.out.println("You have not added any companies. Please add a company first.");
+                } else {
+                    displayCompanies();
+                    System.out.print("Enter the company number you want to sell shares in: ");
+                    userNumberInput = input.nextInt();
+                    input.nextLine();
 
+                    Company currentCompany = userPortfolio.getCompanies().get(userNumberInput-1);
+
+                    if (currentCompany.getNumberOfStocks() == 0) {
+                        System.out.println("You have no shares in this company.");
+                    } else {
+                        displayShares(currentCompany);
+                        int shareToSell = input.nextInt();
+                        input.nextLine();
+
+                        sellShare(currentCompany, shareToSell);
+                    }
+
+
+                }
             } else if (userNumberInput == 5) { // Update a company's stock price
 
             } else if (userNumberInput == 6) { // View profits
@@ -76,9 +114,24 @@ public class StockPortfolioTrackerApp {
             System.out.println(position + ". " + c.getName() + ". Number of shares: " + c.getNumberOfStocks());
             position++;
         }
+    }
 
+    public void addShares(Company company, int numberShares, int price) {
+        for (int i=0; i<numberShares; i++) {
+            company.buyStock(new Stock(price));
+        }
+    }
 
-        
+    public void displayShares(Company company) {
+        int index = 1;
+        for (Stock share : company.getStocks()) {
+            System.out.println(index + ". A stock bought at $" + share.getPriceWhenBought() + ". A profit of $" + share.getProfit());
+            index++;
+        }
+    }
+
+    public void sellShare(Company company, int index) {
+        company.sellStock(index-1);
     }
 
 }
