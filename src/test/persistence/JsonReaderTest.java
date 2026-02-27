a21ad0fd9111de3f143bf6b7386dac93a8cd7775
Jordan Chan <ijordanc@students.cs.ubc.ca>
2026-02-27 01:33:53 -0800

    added some test to JsonReader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -9,9 +9,37 @@ import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExcludeFromJacocoGeneratedReport
-public class JsonReaderTest {
+public class JsonReaderTest extends JsonTest{
     
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/FileDoesNotExist.json");
+        try {
+            Portfolio p = reader.read();
+            fail("File does not exist, so it shoud not have pass");
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Test
+    void testReaderEmptyPortfolio() {
+        JsonReader reader = new JsonReader("./data/FileDoesNotExist.json");
+        try {
+            Portfolio p = reader.read();
+            assertEquals(0, p.getMoneyInvested());
+            assertEquals(0, p.getProfit());
+            assertEquals(0, p.getCompanies().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+
+
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -12,6 +12,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExcludeFromJacocoGeneratedReport
-public class JsonWriterTest {
+public class JsonWriterTest extends JsonTest {
     
 }
