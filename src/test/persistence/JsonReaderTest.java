8b51cde368a5fdb955342e095d159b38fc14d1be
Jordan Chan <ijordanc@students.cs.ubc.ca>
2026-02-28 15:05:47 -0800

    added more tests to JsonReader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -7,6 +7,7 @@ import model.Stock;
 import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -42,8 +43,24 @@ public class JsonReaderTest extends JsonTest{
 
     @Test
     public void testReaderGeneralPortfolio() {
-        JsonReader reader = new JsonReader("./data/testReaderEmptyPortfolio.json");
-        
+        JsonReader reader = new JsonReader("./data/testReaderGeneralPortfolio.json");
+        try {
+            Portfolio portfolio = reader.read();
+            assertEquals(400, portfolio.getMoneyInvested());
+            assertEquals(0, portfolio.getProfit());
+
+            ArrayList<Company> companies = portfolio.getCompanies();
+            Company company1 = companies.get(0);
+            assertEquals(1, companies.size());
+            checkCompany("Tesla", company1.getStocks(), 400, company1);
+
+            checkStock(200, 200, company1.getStocks().get(0));
+            checkStock(200, 200, company1.getStocks().get(1));
+
+
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
     }
 
 
